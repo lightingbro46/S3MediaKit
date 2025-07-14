2aa89e6b8ee092c3bda910d5e882c3ed16279148
anhnq <quanganh10031996@gmail.com>
2025-07-14 22:21:43 +0700

    xử lý lỗi khi demuxer empty

--- manager/Local/TimeQuery.cpp
+++ manager/Local/TimeQuery.cpp
@@ -78,19 +78,22 @@ void TimeQuery::getRecordedTimePeriod(uint64_t start_time, uint64_t end_time,
         lock_guard<recursive_mutex> lck(_mtx);
         
         vector<TimeRange> result;
-        query(start_time, end_time, [&](const TimeBlock &block) {
-            if (!result.empty()) {
-                TimeRange &last = result.back();
-                uint64_t last_end = last.startTime + last.duration;
+        try {
+            query(start_time, end_time, [&](const TimeBlock &block) {
+                if (!result.empty()) {
+                    TimeRange &last = result.back();
+                    uint64_t last_end = last.startTime + last.duration;
 
-                if (block.start_time() <= last_end + 1) {
-                    uint64_t new_end = MAX(last_end, block.start_time() + block.time_len());
-                    last.duration = static_cast<int>(new_end - last.startTime);
-                    return;
+                    if (block.start_time() <= last_end + 1) {
+                        uint64_t new_end = MAX(last_end, block.start_time() + block.time_len());
+                        last.duration = static_cast<int>(new_end - last.startTime);
+                        return;
+                    }
                 }
-            }
-            result.push_back({ block.start_time(), static_cast<uint32_t>(block.time_len()) });
-        });
+                result.push_back({ block.start_time(), static_cast<uint32_t>(block.time_len()) });
+            });
+        } catch(...) {}
+        
         cb(result);
 }
 
@@ -99,21 +102,24 @@ void TimeQuery::getRecordedTimePeriod(uint64_t start_time, uint64_t end_time,
         lock_guard<recursive_mutex> lck(_mtx);
         
         unordered_map<string /*stream*/, vector<TimeRange>> result;
-        query(start_time, end_time, [&](const TimeBlock &block) {
-            auto &range_map = result[block.stream()];
+        try {
+            query(start_time, end_time, [&](const TimeBlock &block) {
+                auto &range_map = result[block.stream()];
 
-            if (!range_map.empty()) {
-                TimeRange &last = range_map.back();
-                uint64_t last_end = last.startTime + last.duration;
+                if (!range_map.empty()) {
+                    TimeRange &last = range_map.back();
+                    uint64_t last_end = last.startTime + last.duration;
 
-                if (block.start_time() <= last_end + 1) {
-                    uint64_t new_end = MAX(last_end, block.start_time() + block.time_len());
-                    last.duration = static_cast<int>(new_end - last.startTime);
-                    return;
+                    if (block.start_time() <= last_end + 1) {
+                        uint64_t new_end = MAX(last_end, block.start_time() + block.time_len());
+                        last.duration = static_cast<int>(new_end - last.startTime);
+                        return;
+                    }
                 }
-            }
-            range_map.push_back({ block.start_time(), static_cast<uint32_t>(block.time_len())});
-        });
+                range_map.push_back({ block.start_time(), static_cast<uint32_t>(block.time_len())});
+            });
+        } catch (...) {}
+       
         cb(result);
 }
 
@@ -122,21 +128,24 @@ void TimeQuery::getRecordedTimePeriod(uint64_t start_time, uint64_t end_time,
         lock_guard<recursive_mutex> lck(_mtx);
 
         unordered_map<string /*date*/, set<int /*hour*/>> result;
-        query(start_time, end_time, [&](const TimeBlock &block) {
-            string date_str = getTimeStr("%Y-%m-%d", block.start_time());
-            string hour_str = getTimeStr("%H", block.start_time());
+        try {
+            query(start_time, end_time, [&](const TimeBlock &block) {
+                string date_str = getTimeStr("%Y-%m-%d", block.start_time());
+                string hour_str = getTimeStr("%H", block.start_time());
 
-            auto &hour_map = result[date_str];
-            hour_map.emplace(static_cast<int>(atoi(hour_str.data())));
+                auto &hour_map = result[date_str];
+                hour_map.emplace(static_cast<int>(atoi(hour_str.data())));
 
-            auto block_end_time = block.start_time() + block.time_len();
-            if (block_end_time < end_time && getStartOfHour(block.start_time()) < getStartOfHour(block_end_time)) {
-                string next_date_str = getTimeStr("%Y-%m-%d", block_end_time);
-                string next_hour_str = getTimeStr("%H", block_end_time);
-                auto &next_hour_map = result[next_date_str];
-                next_hour_map.emplace(static_cast<int>(atoi(next_hour_str.data())));
-            }
-        });
+                auto block_end_time = block.start_time() + block.time_len();
+                if (block_end_time < end_time && getStartOfHour(block.start_time()) < getStartOfHour(block_end_time)) {
+                    string next_date_str = getTimeStr("%Y-%m-%d", block_end_time);
+                    string next_hour_str = getTimeStr("%H", block_end_time);
+                    auto &next_hour_map = result[next_date_str];
+                    next_hour_map.emplace(static_cast<int>(atoi(next_hour_str.data())));
+                }
+            });
+        } catch (...) {}
+
         cb(result);
 }
 
@@ -145,33 +154,36 @@ void TimeQuery::getRecordedTimePeriod(uint64_t start_time, uint64_t end_time,
         lock_guard<recursive_mutex> lck(_mtx);
 
         unordered_map<string /*stream_id*/, unordered_map<string /*date*/, unordered_map<int, std::vector<TimeRange>>>> result;
-        query(start_time, end_time, [&](const TimeBlock &block) {
-            string stream_id = block.stream();
-            uint64_t current = block.start_time();
-            uint32_t remaining = block.time_len();
+        try {
+            query(start_time, end_time, [&](const TimeBlock &block) {
+                string stream_id = block.stream();
+                uint64_t current = block.start_time();
+                uint32_t remaining = block.time_len();
 
-            while(remaining > 0) {
-                string date_str = getTimeStr("%Y-%m-%d", current);
-                string hour_str = getTimeStr("%H", current);
-                string minute_str = getTimeStr("%M", current);
-                string second_str = getTimeStr("%S", current);
+                while(remaining > 0) {
+                    string date_str = getTimeStr("%Y-%m-%d", current);
+                    string hour_str = getTimeStr("%H", current);
+                    string minute_str = getTimeStr("%M", current);
+                    string second_str = getTimeStr("%S", current);
 
-                int64_t start_of_hour = getStartOfHour(current);
-                uint32_t offset_in_hour = static_cast<uint32_t>(current - start_of_hour);
-                uint32_t seconds_left_in_hour = 3600 - offset_in_hour;
-                uint32_t chunk = MIN(remaining, seconds_left_in_hour);
-                
-                auto &range_map = result[stream_id][date_str][static_cast<int>(atoi(hour_str.data()))];
-                auto chunk_start = current;
-                if (!range_map.empty() && range_map.back().startTime + range_map.back().duration + 1 >= chunk_start) {
-                    range_map.back().duration += chunk;
-                } else {
-                    range_map.push_back({chunk_start, chunk});
+                    int64_t start_of_hour = getStartOfHour(current);
+                    uint32_t offset_in_hour = static_cast<uint32_t>(current - start_of_hour);
+                    uint32_t seconds_left_in_hour = 3600 - offset_in_hour;
+                    uint32_t chunk = MIN(remaining, seconds_left_in_hour);
+                    
+                    auto &range_map = result[stream_id][date_str][static_cast<int>(atoi(hour_str.data()))];
+                    auto chunk_start = current;
+                    if (!range_map.empty() && range_map.back().startTime + range_map.back().duration + 1 >= chunk_start) {
+                        range_map.back().duration += chunk;
+                    } else {
+                        range_map.push_back({chunk_start, chunk});
+                    }
+                    current += chunk;
+                    remaining -= chunk;
                 }
-                current += chunk;
-                remaining -= chunk;
-            }
-        });
+            });
+        } catch (...) {}
+        
         cb(result);
     }
 
@@ -180,25 +192,31 @@ void TimeQuery::getRecordedTimePeriod(uint64_t start_time, uint64_t end_time,
         lock_guard<recursive_mutex> lck(_mtx);
 
         vector<TimeBlock> result;
-        query(start_time, end_time, [&](const TimeBlock &block) {
-            result.push_back(block); 
-        });
+        try {
+            query(start_time, end_time, [&](const TimeBlock &block) {
+                result.push_back(block); 
+            });
+        } catch (...) {}
+        
         cb(result);
 }
 
 int64_t TimeQuery::getOffsetOfDate(uint64_t pos_time) {
     lock_guard<recursive_mutex> lck(_mtx);
     int64_t ret = 0;
-    auto start_of_date = getStartOfDay(pos_time);
-    query(start_of_date, pos_time, [&](const TimeBlock &block) {
-        if (block.start_time() < start_of_date && block.start_time() + block.time_len() > start_of_date) {
-            ret +=  block.start_time() + block.time_len() - start_of_date;
-        } else if (block.start_time() >= start_of_date && block.start_time() + block.time_len() <= pos_time){ 
-            ret += block.time_len();
-        }  if (block.start_time() <= pos_time && block.start_time() + block.time_len() > pos_time) {
-            ret += pos_time - block.start_time();
-        }
-    });
+    try {
+        auto start_of_date = getStartOfDay(pos_time);
+        query(start_of_date, pos_time, [&](const TimeBlock &block) {
+            if (block.start_time() < start_of_date && block.start_time() + block.time_len() > start_of_date) {
+                ret +=  block.start_time() + block.time_len() - start_of_date;
+            } else if (block.start_time() >= start_of_date && block.start_time() + block.time_len() <= pos_time){ 
+                ret += block.time_len();
+            }  if (block.start_time() <= pos_time && block.start_time() + block.time_len() > pos_time) {
+                ret += pos_time - block.start_time();
+            }
+        });
+    } catch (...) {}
+    
     return ret;
 }
 
@@ -207,17 +225,20 @@ std::shared_ptr<TimeBlock> TimeQuery::getLastBlock(uint32_t interval_sec) {
     uint64_t pos = time(nullptr);
     bool has_block = false;
     std::shared_ptr<TimeBlock> last_block;
-    uint64_t first_time = _demuxer->getFirstStamp();
-    while (!has_block && pos > first_time) {
-        auto start_pos = pos - interval_sec;
-        query(start_pos, pos, [&](const TimeBlock &block) { 
-            has_block = true;
-            last_block = std::make_shared<TimeBlock>(block);
-        });
-        if (!has_block) {
-            pos = start_pos;
+    try {
+        uint64_t first_time = _demuxer->getFirstStamp();
+        while (!has_block && pos > first_time) {
+            auto start_pos = pos - interval_sec;
+            query(start_pos, pos, [&](const TimeBlock &block) { 
+                has_block = true;
+                last_block = std::make_shared<TimeBlock>(block);
+            });
+            if (!has_block) {
+                pos = start_pos;
+            }
         }
-    }
+    } catch (...) {}
+    
     return has_block ? last_block : nullptr;
 }
 
