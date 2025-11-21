5f6781bdfca2fa1550cb3154708ff181152e22e1
anhnq <quanganh10031996@gmail.com>
2025-11-21 09:34:21 +0700

    fix bug replace ip bị duplicate ip khi ko có port

--- manager/Extension/Plugin.cpp
+++ manager/Extension/Plugin.cpp
@@ -12,12 +12,14 @@ namespace managerkit {
 string replaceIp(const string &in_url, const string &nat_ip) {
     auto schema = findSubString(in_url.data(), nullptr, "://");
     auto middle_url = findSubString(in_url.data(), "://", "/");
-    auto _middle_url = middle_url;
+    string _middle_url;
     auto it = middle_url.rfind(":");
     if (it != string::npos) {
         _middle_url = middle_url.substr(it);
+        _middle_url = nat_ip + _middle_url;
+    } else {
+        _middle_url = nat_ip;
     }
-    _middle_url = nat_ip + _middle_url;
     auto path = findSubString(in_url.data() + schema.size() + middle_url.size() + 4, nullptr, nullptr);
     return (StrPrinter << schema << "://" << _middle_url << "/" << path);
 }
